4de3e740294d6efb2e55902a8367da1f471ff620
MaybeHawk1 <bardiarone90124@gmail.com>
2024-07-13 15:08:50 +0330

    "Added install.ps1"

---
+++ install.ps1
@@ -0,0 +1,13 @@
+winget install JanDeDobbeleer.OhMyPosh
+winget install junegunn.fzf
+winget install git.git
+winget install neovim.neovim
+winget install BurntSushi.ripgrep.MSVC
+winget install Microsoft.WindowsTerminal
+winget install "Flow Launcher"
+winget install OpenJS.NodeJS.LTS
+winget install JesseDuffield.lazygit
+
+git clone https://github.com/MaybeHawk1/dotfiles.git
+cd dotfiles
+ls
