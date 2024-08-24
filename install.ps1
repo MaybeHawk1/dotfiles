8eab1c8c1417fae09d4205baff1960125de0c89d
MaybeHawk1 <bardiarone90124@gmail.com>
2024-08-24 11:49:25 +0330

    Updated install.ps1

--- install.ps1
+++ install.ps1
@@ -1,9 +1,4 @@
-winget install starship.starship
-winget install junegunn.fzf
-winget install git.git
-winget install neovim.neovim
-winget install BurntSushi.ripgrep.MSVC
-winget install Microsoft.WindowsTerminal
-winget install "Flow Launcher"
-winget install OpenJS.NodeJS.LTS
-winget install JesseDuffield.lazygit
+scoop install neovim
+scoop install fzf
+scoop install lazygit
+scoop install vscodium
