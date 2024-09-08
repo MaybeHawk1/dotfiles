8001190a5d99e91bc0383268e7cbc9a3a2e2701a
MaybeHawk1 <bardiarone90124@gmail.com>
2024-09-08 09:52:50 -0700

    Updated Neovim And PowerShell Config

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -4,7 +4,7 @@ Set-Alias vim nvim
 Set-Alias cc clear
 Set-Alias tt tree
 #Prompt
-oh-my-posh init pwsh --config ~/Documents/PowerShell/Themes/darkblood.omp.json | Invoke-Expression 
+oh-my-posh init pwsh --config ~/Documents/PowerShell/Themes/robbyrussell.omp.json | Invoke-Expression
 
 #Functions
 function whereis ($command) {
