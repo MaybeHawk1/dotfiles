4f59e9e13f7644424ee52d37d8b991800b79fea6
MaybeHawk1 <bardiarone90124@gmail.com>
2024-09-09 01:45:00 -0700

    Updated install.ps1 and wezterm config

--- install.ps1
+++ install.ps1
@@ -1,3 +1,13 @@
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+
+scoop bucket add extras
+
+scoop install vcredist2022
+scoop install ripgrep
+scoop install nodejs
+scoop install wezterm
+scoop install pwsh
 scoop install neovim
 scoop install fzf
 scoop install lazygit
--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,13 +1,16 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
-config.default_prog = { 'C:\\Program Files\\PowerShell\\7\\pwsh.exe' }
-config.color_scheme = 'Catppuccin Mocha'
+config.default_prog = { 'C:\\Users\\MaybeHawk\\scoop\\apps\\pwsh\\7.4.5\\pwsh.exe' }
+config.color_scheme = 'Ashes (base16)'
 config.default_cwd = "~"
 
 config.font = wezterm.font 'JetBrainsMono Nerd Font'
 config.font_size = 11
 
 config.hide_tab_bar_if_only_one_tab = true
+
+config.window_background_opacity = 1
+config.win32_system_backdrop = 'Acrylic'
 
 return config
